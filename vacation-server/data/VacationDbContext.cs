7e24736ca0d0bba6cff5a0997ad156ccc809e910
alijanbein <alijanbeincs@gmail.com>
2023-06-18 10:15:15 +0300

    Backend: added VacationdbContext file with relations

---
+++ vacation-server/data/VacationDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+public class VacationDbContext : DbContext
+{
+    public DbSet<Employee>? Employees {get; set;}
+    public DbSet<Vacation>? Vacations {get; set;}
+
+       public VacationDbContext(DbContextOptions<VacationDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Employee>().ToTable("Employees");
+    modelBuilder.Entity<Vacation>().ToTable("Vacations");
+
+    modelBuilder.Entity<Vacation>()
+    .HasOne(v => v.Employee)
+    .WithMany(e => e.Vacations)
+    .HasForeignKey(v => v.EmployeeId);
+}
+}
--- vacation-server/models/Employe.cs
+++ vacation-server/models/Employe.cs
@@ -3,5 +3,7 @@ public class Employee
     public int EmployeeId {get; set;}
     public string? Username {get; set;}
     public string? Password {get; set;}
+
+    public ICollection<Vacation>? Vacations { get; set; }
 }
 
