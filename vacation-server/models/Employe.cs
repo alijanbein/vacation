a38b98e231ad9544005ddfe7eef614900c8ce53a
alijanbein <alijanbeincs@gmail.com>
2023-06-18 09:56:58 +0300

    Backend: added vacation model

--- vacation-server/models/Employe.cs
+++ vacation-server/models/Employe.cs
@@ -1,4 +1,5 @@
-public class Employee {
+public class Employee
+ {
     public int EmployeeId {get; set;}
     public string? Username {get; set;}
     public string? Password {get; set;}
---
+++ vacation-server/models/Vactaion.cs
@@ -0,0 +1,9 @@
+public class Vacation
+{
+    public int VacationId { get; set; }
+    public string? Description { get; set; }
+    public DateTime DateTimeFrom { get; set; }
+    public DateTime DateTimeTo { get; set; }
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; }
+}
