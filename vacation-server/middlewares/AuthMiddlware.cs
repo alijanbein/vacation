0cb34d5a7c3a0eca8ffca171090cdd6dcf4ade41
alijanbein <alijanbeincs@gmail.com>
2023-06-19 13:35:14 +0300

    Frontend: added frontend start code

--- vacation-server/middlewares/AuthMiddlware.cs
+++ vacation-server/middlewares/AuthMiddlware.cs
@@ -50,6 +50,7 @@ public class AuthMiddelware
                     await context.Response.WriteAsync("your are not authorized");
 
                     return;
+                    
                 }
             }
             else
